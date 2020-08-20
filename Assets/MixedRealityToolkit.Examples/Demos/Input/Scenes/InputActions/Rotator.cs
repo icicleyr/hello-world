f3c7a156ed0768d905fc3047ffafcb64fbf0ce9c
e <2223873689@qq.com>
2020-08-20 11:58:52 +0800

    测试提交

---
+++ Assets/MixedRealityToolkit.Examples/Demos/Input/Scenes/InputActions/Rotator.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See LICENSE in the project root for license information.
+
+using UnityEngine;
+
+namespace Microsoft.MixedReality.Toolkit.Examples.Demos
+{
+    // Silly script that applies a rotation about the up axis on demand.
+    public class Rotator : MonoBehaviour
+    {
+        public float angle = 45f;
+
+        public void Rotate()
+        {
+            transform.Rotate(0, angle, 0);
+        }
+
+        public void Scale()
+        {
+            transform.localScale = 0.5f * Vector3.one;
+        }
+    }
+}
